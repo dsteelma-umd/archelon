06ce4e0b833077646f1c202c17ae0f0d8769240c
David P. Steelman <dsteelma@umd.edu>
2024-04-25 08:53:28 -0400

    SSDR_R_AND_D_20240425. Import HTMX
    Added HTMX to "application.js" so that it is usable in Rails ERB files.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,6 @@
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+// Import HTMX
+import "htmx.org"
