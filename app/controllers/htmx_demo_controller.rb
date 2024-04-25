4968a0c8f638d2ec1f035e1bf26253df0cce8406
David P. Steelman <dsteelma@umd.edu>
2024-04-25 13:19:06 -0400

    SSDR_R_AND_D_20240425. TypeLiteral HTMX proof-of-concept
    An HTMX proof-of-concept for the "TypedLiteral" component.
    
    Uses hidden input fields to pass information about the value type, and
    RDF subject, predicate, object, to send information back to HTMX,
    along with the input field value to construct the "insert" and "delete"
    input fields needed to generate the SPARQL query.

--- app/controllers/htmx_demo_controller.rb
+++ app/controllers/htmx_demo_controller.rb
@@ -2,6 +2,20 @@ class HtmxDemoController < ApplicationController
   def index
   end
   def replace
-    render html: '<div>Button was clicked</div>'.html_safe
+    subject = params['subject']
+    predicate = params['predicate']
+    original_value = params['original_value']
+    new_value = params['new_value']
+    value_type = params['value_type']
+
+    render html: """
+    <input type='hidden' name='value_type' value='#{value_type}'>
+    <input type='hidden' name='subject' value='#{subject}'>
+    <input type='hidden' name='predicate' value='#{predicate}'>
+    <input type='hidden' name='original_value' value='#{original_value}'>
+
+    <input type='hidden' name='delete[]' value='<#{subject}> <#{predicate}> &quot;#{original_value}&quot;^^<#{value_type}> .'>
+    <input type='hidden' name='insert[]' value='<#{subject}> <#{predicate}> &quot;#{new_value}&quot;^^<#{value_type}> .'>
+    """.html_safe
   end
  end
